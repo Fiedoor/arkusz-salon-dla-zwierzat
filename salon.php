cbb19d702cd97ffe9686417962286f9b0da06957
Fiedor_ <stachfie@wp.pl>
2023-09-17 18:21:59 +0200

    Bez PHP

--- salon.php
+++ salon.php
@@ -1,1 +1,39 @@
-400
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Salon pielęgnacji</title>
+    <link rel="stylesheet" href="salon.css">
+</head>
+
+<body>
+    <header>
+        <h1>SALON PIELĘGNACJI PSÓW I KOTÓW</h1>
+    </header>
+    <main>
+        <div class="side">
+            <h3>SALON ZAPRASZA W DNIACH</h3>
+            <ul>
+                <li>Poniedziałek, 12:00-18:00</li>
+                <li>Wtorek, 12:00-18:00</li>
+            </ul>
+            <a href="pies.jpeg"> <img src="pies-mini.jpeg"></a>
+            <p>Umów się telefonicznie na wizytę lub po prostu przyjdź!</p>
+        </div>
+        <div id="mid">
+            <h3>PRZYPOMNIENIE O NASTĘPNEJ WIZYCIE</h3>
+            <?php
+            //skrypt 1
+            ?>
+        </div>
+        <div class="side">
+            <h3>USŁUGI</h3>
+            <?php
+            //skrypt 2
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
